bad57b1798263d95d7d943f3462da73889527417
trsv.egor <eg0r-taras0v@yandex.ru>
2025-07-27 10:12:10 +0300

    Убран ненужный код (для тестирования сетевого клиента)

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,24 +1,13 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
-
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.data.network.impl.VacanciesRepositoryImpl
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
-import ru.practicum.android.diploma.domain.api.VacanciesInteractor
-import ru.practicum.android.diploma.domain.impl.VacanciesInteractorImpl
-import ru.practicum.android.diploma.domain.models.Vacancy
 
-class RootActivity (): AppCompatActivity() {
-
-    private val intertactor: VacanciesInteractor by inject()
+class RootActivity() : AppCompatActivity() {
 
     private var _binding: ActivityRootBinding? = null
     private val binding get() = _binding!!
@@ -34,17 +23,5 @@ class RootActivity (): AppCompatActivity() {
 
         binding.bottomNavigationView.setupWithNavController(navController)
 
-
-
-        var pair = Pair(listOf<Vacancy>(), "")
-
-        lifecycleScope.launch {
-            intertactor.searchVacancies("UX", 0)
-                .collect { item ->
-                    pair = Pair(item.first.orEmpty(), item.second.orEmpty())
-                }
-            Log.i("HH response data", pair.first.toString())
-            Log.i("HH response error", pair.second)
-        }
     }
 }
