307915709e64747d93f5d1ba3b93d87226d63cc3
Sergey Kravtsov <kraffc@yandex.ru>
2025-07-30 22:44:47 +0300

    баг фикс

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/favourite/FavouriteRepository.kt
@@ -0,0 +1,14 @@
+package ru.practicum.android.diploma.domain.api.favourite
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.domain.models.VacancyDetail
+
+interface FavouriteRepository {
+    suspend fun addToFavourites(vacancy: VacancyDetail)
+    suspend fun removeFromFavourites(id: String)
+    suspend fun updateFavouriteJob()
+    fun getJobById(id: String): Flow<String?>
+    fun getFavourites(): Flow<List<Vacancy>>
+    fun checkJobInFavourites(id: String): Flow<Boolean>
+}
