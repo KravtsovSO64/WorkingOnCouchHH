94381aaf6966f9381658302726e6a4a7e1c021f5
trsv.egor <trsv.egor@gmail.com>
2025-07-31 00:53:29 +0300

    iteration_2_review Исправление на основании комментариев detekt 3

--- app/src/main/java/ru/practicum/android/diploma/data/network/impl/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/impl/RetrofitNetworkClient.kt
@@ -8,9 +8,13 @@ import ru.practicum.android.diploma.data.dto.Response
 import ru.practicum.android.diploma.data.dto.VacanciesRequest
 import ru.practicum.android.diploma.data.dto.VacancyDetailRequest
 import ru.practicum.android.diploma.data.network.api.YandexVacanciesApi
+import ru.practicum.android.diploma.data.network.impl.VacanciesRepositoryImpl.Companion.REQ_TIMEOUT
+import ru.practicum.android.diploma.data.network.impl.VacanciesRepositoryImpl.Companion.UNAUTHORIZED
 import ru.practicum.android.diploma.data.network.interfaces.NetworkClient
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
+import ru.practicum.android.diploma.data.network.impl.VacanciesRepositoryImpl.Companion.UNKNW_HOST
+
 
 class RetrofitNetworkClient(
     private val yandexVacanciesApi: YandexVacanciesApi
@@ -19,29 +23,27 @@ class RetrofitNetworkClient(
     override suspend fun doRequest(dto: Any): Response {
         try {
             return withContext(Dispatchers.IO) {
-             when (dto) {
-                is VacanciesRequest ->
+                when (dto) {
+                    is VacanciesRequest ->
                         yandexVacanciesApi.getVacancies(
                             dto.text,
                             dto.page,
                         ).apply { resultCode = VacanciesRepositoryImpl.Companion.NET_SUCCESS }
 
-                is VacancyDetailRequest ->
+                    is VacancyDetailRequest ->
                         yandexVacanciesApi.getVacancyDetails(
                             dto.id,
                         ).apply { resultCode = VacanciesRepositoryImpl.Companion.NET_SUCCESS }
 
-                else -> badRequest()}
+                    else -> badRequest()
+                 }
             }
-        } catch (e: UnknownHostException) {
-            Log.e("NetworkClient ERROR: No Connection", e.message.orEmpty())
-            Response().apply { resultCode = VacanciesRepositoryImpl.Companion.UNKNW_HOST }
-        } catch (e: SocketTimeoutException) {
-            Log.e("NetworkClient ERROR: Timeout", e.message.orEmpty())
-            Response().apply { resultCode = VacanciesRepositoryImpl.Companion.REQ_TIMEOUT }
-        } catch (e: HttpException) {
-            Log.e("NetworkClient ERROR: Token", e.message.orEmpty())
-            Response().apply { resultCode = VacanciesRepositoryImpl.Companion.UNAUTHORIZED }
+        } catch (error: UnknownHostException) {
+            errorHandler(error, UNKNW_HOST)
+        } catch (error: SocketTimeoutException) {
+            errorHandler(error, REQ_TIMEOUT)
+        } catch (error: HttpException) {
+            errorHandler(error, UNAUTHORIZED)
         }
         return badRequest()
     }
@@ -49,6 +51,13 @@ class RetrofitNetworkClient(
     private fun badRequest(): Response {
         return Response().apply {
             resultCode = VacanciesRepositoryImpl.Companion.NET_BAD_REQUEST
+        }
+    }
+
+    private fun errorHandler(error: Exception,code: Int): Response {
+        Log.e("NetworkClient ERROR", error.message.orEmpty())
+        return Response().apply {
+            resultCode = code
         }
     }
 }
