4a30a3fd105967423bd9e8aca2e7fa463d57e1f1
trsv.egor <trsv.egor@gmail.com>
2025-07-30 23:47:46 +0300

    Настроено отображение избранных вакансий

--- app/src/main/java/ru/practicum/android/diploma/data/favourite/FavouriteRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/favourite/FavouriteRepositoryImpl.kt
@@ -1,10 +1,12 @@
 package ru.practicum.android.diploma.data.favourite
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import ru.practicum.android.diploma.data.db.AppDatabase
 import ru.practicum.android.diploma.data.db.FavouriteJob
 import ru.practicum.android.diploma.domain.api.favourite.FavouriteRepository
-import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.domain.models.FilterArea
+import ru.practicum.android.diploma.domain.models.FilterIndustry
 import ru.practicum.android.diploma.domain.models.VacancyDetail
 
 class FavouriteRepositoryImpl(
@@ -27,12 +29,14 @@ class FavouriteRepositoryImpl(
         TODO("Not yet implemented getJobById")
     }
 
-    override fun getFavourites(): Flow<List<Vacancy>> {
-        TODO("Not yet implemented getFavourites")
+    override suspend fun getFavourites(): Flow<List<VacancyDetail>> = flow {
+        emit(appDatabase.favouriteDao().getAll().map {
+            map(it)
+        })
     }
 
     override fun checkJobInFavourites(id: String): Flow<Boolean> {
-       return appDatabase.favouriteDao().isFavorite(id)
+        return appDatabase.favouriteDao().isFavorite(id)
     }
 
     private fun map(vacancy: VacancyDetail): FavouriteJob {
@@ -51,5 +55,37 @@ class FavouriteRepositoryImpl(
             description = vacancy.description,
             url = vacancy.url
         )
+    }
+
+    private fun map(favoruteJob: FavouriteJob): VacancyDetail {
+        return VacancyDetail(
+            id = favoruteJob.id,
+            name = favoruteJob.name.orEmpty(),
+            employerName = favoruteJob.employerName.orEmpty(),
+            salaryFrom = favoruteJob.salaryFrom ?: 0,
+            salaryTo = favoruteJob.salaryTo ?: 0,
+            salaryCurrency = favoruteJob.salaryCurrency.orEmpty(),
+            area = FilterArea(
+                id = 0, name = favoruteJob.area.orEmpty(), areas = listOf(), parentId = 0
+            ),
+            employment = favoruteJob.employment.orEmpty(),
+            schedule = favoruteJob.schedule.orEmpty(),
+            experience = favoruteJob.experience.orEmpty(),
+            skills = favoruteJob.keySkills?.split(", ").orEmpty(),
+            description = favoruteJob.description.orEmpty(),
+            url = favoruteJob.url.orEmpty(),
+            contactsEmail = "",
+            contactsPhone = listOf(),
+            contactsName = "",
+            addressStreet = "",
+            employerLogo = "",
+            industry = FilterIndustry(
+                id = 0, name = ""
+            ),
+            addressCity = "",
+            addressBuilding = "",
+
+
+            )
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/favourite/FavouriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favourite/FavouriteFragment.kt
@@ -4,30 +4,84 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentFavouriteBinding
+import ru.practicum.android.diploma.domain.models.VacancyDetail
+import ru.practicum.android.diploma.presentation.favourites.FavouritesViewModel
+import ru.practicum.android.diploma.presentation.favourites.state.FavouritesState
+import ru.practicum.android.diploma.ui.search.JobAdapter
+import kotlin.getValue
 
 class FavouriteFragment : Fragment() {
 
     private var _binding: FragmentFavouriteBinding? = null
     private val binding get() = _binding!!
 
+    private val favouritesViewModel: FavouritesViewModel by viewModel()
+
+    private var adapterFavourites = FavouriteVacancyAdapter { item ->
+
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(
-        view: View,
-        savedInstanceState: Bundle?
+        view: View, savedInstanceState: Bundle?
     ) {
         super.onViewCreated(view, savedInstanceState)
+        favouritesViewModel.observeState().observe(viewLifecycleOwner) {
+            render(it)
+        }
 
+        binding.recyclerViewFavoriteVacancies.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewFavoriteVacancies.adapter = adapterFavourites
+
+        favouritesViewModel.getFavourites()
     }
+
+    private fun render(state: FavouritesState) {
+        when (state) {
+            is FavouritesState.Empty -> {
+                showEmpty()
+            }
+
+            is FavouritesState.Loading -> {
+                showLoading()
+            }
+
+            is FavouritesState.Favorites -> {
+                showFavorites(state.vacancies)
+            }
+        }
+    }
+
+    private fun showEmpty() {
+        binding.recyclerViewFavoriteVacancies.isVisible = false
+        binding.favouriteEmpty.isVisible = true
+        binding.favouriteError.isVisible = false
+    }
+
+    private fun showLoading() {
+        binding.recyclerViewFavoriteVacancies.isVisible = false
+        binding.favouriteEmpty.isVisible = false
+        binding.favouriteError.isVisible = true
+    }
+
+    private fun showFavorites(list: List<VacancyDetail>) {
+        adapterFavourites.setItems(list)
+        binding.recyclerViewFavoriteVacancies.isVisible = true
+        binding.favouriteEmpty.isVisible = false
+        binding.favouriteError.isVisible = false
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
